f6c41be87c5141519230e6a3c1f36be2c0aab750
Eustia232 <a728076618@163.com>
2026-03-26 17:27:26 +0800

    chore: remove slow PyInstaller packaging tests

--- tests/test_packaging_instructions.py
+++
@@ -1,7 +0,0 @@
-from src.packager import build_exe
-
-
-def test_packaging_returns_code():
-    # we won't actually invoke pyinstaller in CI; ensure function callable
-    rc = build_exe("main.py")
-    assert isinstance(rc, int)
--- tests/test_packaging_smoke.py
+++
@@ -1,7 +0,0 @@
-from src.packager import build_exe
-
-
-def test_packaging_smoke():
-    # ensure build_exe returns int (already covered), keep smoke test for packaging step
-    rc = build_exe("main.py")
-    assert isinstance(rc, int)
